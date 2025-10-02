35d42b8d06850cfe65c46eaf0f8c4ec904ad4e27
Sergio Gordillo <sergiogordillodev@gmail.com>
2025-10-02 18:24:04 +0200

    feat: configuration of routes done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'books-list',
+    loadComponent: () => import('./pods/book/books-list/books-list').then((m) => m.BooksList),
+  },
+  {
+    path: 'books-detail',
+    loadComponent: () => import('./pods/book/books-detail/books-detail').then((m) => m.BooksDetail),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { BooksDetail } from './pods/book/books-detail/books-detail';
+import { BooksList } from './pods/book/books-list/books-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NzButtonModule],
+  imports: [RouterOutlet, NzButtonModule, BooksList, BooksDetail],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('library-app');
